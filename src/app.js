8724b7b80493923ffaf49d9fdff61a23c251b1e3
Emanuel Ruiz <emahrui@hotmail.com>
2023-06-20 20:48:56 -0300

    inicio de preentrega numero dos.

---
+++ src/app.js
@@ -0,0 +1,97 @@
+import express from "express";
+import handlebars from "express-handlebars";
+import mongoose from "mongoose";
+import { Server } from "socket.io";
+import ProductManager from "./DAO/productsDAO.js";
+import MessagesManager from "./DAO/messagesDAO.js";
+import cartRouter from "./routes/cart.routes.js";
+import homeRouter from "./routes/home.routes.js";
+import productsRouter from "./routes/products.routes.js";
+import realTimeRoutes from "./routes/realTime.routes.js";
+import messagesRouter from "./routes/messages.routes.js";
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.engine(
+    "handlebars",
+    handlebars.engine({
+        runtimeOptions: {
+            allowProtoPropertiesByDefault: true,
+            allowProtoMethodsByDefault: true,
+        },
+    })
+);
+app.set("views", "./src/views");
+app.set("view engine", "handlebars");
+app.use("/api/products", productsRouter);
+app.use("/api/cart", cartRouter);
+app.use("/", homeRouter);
+app.use("/realtimeproducts", realTimeRoutes);
+app.use("/chat", messagesRouter);
+
+const server = app.listen(8080, () =>
+    console.log("Corriendo en el puerto: 8080")
+);
+
+mongoose.connect(
+    "mongodb+srv://Coder:spiderverse2099@coder.qugttqn.mongodb.net/ecommerce",
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    }
+);
+
+const db = mongoose.connection;
+
+db.on("error", (error) => {
+    console.error("Error de conexión:", error);
+});
+
+db.once("open", () => {
+    console.log("Conexión exitosa a la base de datos.");
+});
+
+const io = new Server(server);
+const manager = new ProductManager();
+const managerMsg = new MessagesManager();
+const message = [];
+
+io.on("connection", async (socket) => {
+    console.log("nuevo cliente conectado");
+    const products = await manager.getProducts();
+    io.emit("productList", products);
+    socket.on("product", async (newProd) => {
+        const result = await manager.addProduct(newProd);
+        if (result.error) {
+            socket.emit("productAddError", result.error);
+        } else {
+            const products = await manager.getProducts();
+            io.emit("productList", products);
+            socket.emit("productAddSuccess");
+        }
+    });
+
+    socket.on("productDelete", async (delProduct) => {
+        try {
+            let pid = await manager.deleteProduct(delProduct);
+            const products = await manager.getProducts();
+            io.emit("productList", products);
+        } catch (error) {
+            socket.emit("productDeleteError", error.message);
+        }
+    });
+
+    socket.on("messages", async (data) => {
+        let msgSend;
+        try {
+            msgSend = await managerMsg.addMessage(data);
+            message.unshift(data);
+            io.emit("messageLogs", message);
+        } catch (error) {
+            throw error;
+        }
+    });
+});
---
+++ src/routes/products.routes.js
@@ -0,0 +1,82 @@
+import { Router } from "express";
+import ProductManager from "../DAO/productsDAO.js";
+import { validateAddProduct } from "../utils/index.js";
+const productsRouter = Router();
+
+const manager = new ProductManager();
+
+productsRouter.get("/", async (req, res) => {
+    try {
+        let limit = req.query.limit;
+        let products = await manager.getProducts();
+
+        if (limit) {
+            let limitQuery = products.reverse().slice(0, limit);
+            return res.send(limitQuery);
+        }
+        res.send(products);
+    } catch (error) {
+        throw error;
+    }
+});
+
+productsRouter.get("/:pid", async (req, res) => {
+    let pid = req.params.pid;
+    let product = await manager.getProductById(pid);
+    if (!product) {
+        return res
+            .status(400)
+            .send({ status: "error", error: "Producto inexistente" });
+    }
+    res.send(product);
+});
+
+productsRouter.post("/", async (req, res) => {
+    let product = req.body;
+    if (!validateAddProduct(product)) {
+        res
+            .status(400)
+            .send({ status: "error", msg: "Por favor, completá todos los datos" });
+    } else {
+        try {
+            await manager.addProduct(product);
+            res
+                .status(200)
+                .send({ status: "success", msg: "Producto agregado exitosamente" });
+        } catch (error) {
+            res.status(400).send({
+                status: "error",
+                msg: `El código ${product.code} ya fue ingresado, por favor ingresa otro diferente`,
+            });
+        }
+    }
+});
+
+productsRouter.put("/:pid", async (req, res) => {
+    let pid = req.params.pid;
+    let fields = req.body;
+    try {
+        let updatedProduct = await manager.updateProduct(pid, fields);
+        if (!updatedProduct) {
+            res.status(404).send({ status: "error", msg: "El producto no existe" });
+        }
+        res.send({
+            status: "successful",
+            msg: "Producto modificado correctamente",
+        });
+    } catch (error) {
+        throw error;
+    }
+});
+
+productsRouter.delete("/:pid", async (req, res) => {
+    const pid = req.params.pid;
+    const deletedProduct = await manager.deleteProduct(pid);
+    if (!deletedProduct) {
+        res.status(404).send({ status: "error", msg: "El producto no existe" });
+    } else {
+        res.send({ status: "successful", msg: "Producto eliminado correctamente" });
+    }
+});
+
+export default productsRouter;
